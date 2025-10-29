1e09b6677d0a7fb97323832acc996f2fd0a1a818
Yousef El-Maghrabi <yousefmaghrabi2009@gmail.com>
2025-10-30 02:46:43 +0300

    created data directory, images system.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello There")
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 5000;
+
+app.use(cors())
+app.use("/public/images", express.static("public/images"));
+
+// Example route
+app.get("/", (req, res) => {
+    res.render("/public/pages/index.html")
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
