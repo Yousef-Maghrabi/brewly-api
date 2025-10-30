0a1c3aa1a366e0d6b41d092a30184362618d130c
Yousef El-Maghrabi <yousefmaghrabi2009@gmail.com>
2025-10-30 20:13:17 +0300

    philips machines simple data

---
+++ data/machines.js
@@ -0,0 +1,94 @@
+const data = {
+    philips: {
+            pmc01: {
+                title: "philips 4300 series latte go",
+                details: "very milky",
+                price: 400.99,
+                imagePath: "/public/images/machines/philips/4300-lattego/01.webp ",
+                keySpecs : {
+                    coffeeTypes: "Espresso, Coffee, Americano, Latte Macchiato, Cappuccino, and more.",
+                    milkSystem: "LatteGo, which includes a two-part milk system for easy cleaning.",
+                    coffeeStrengthSettings: "Adjustable from mild to strong.",
+                    Grinder: "Integrated ceramic grinder with 12 settings for customizable grind size.",
+                    waterTankCapacity: "Approximately 18.5L",
+                    beanContainerCapacity: "About 250g",
+                    Dimensions: " Approximately 24 x 37 x 43 cm (W x D x H).",
+                    Weight: "Around 7.5 kg.",
+                    powerConsumption: "1500 watts."
+                },
+                features: {
+                    oneTouchFunctionality: "Allows for quick preparation of various coffee drinks.",
+                    easyCleaning: "Removable brewing group and dishwasher-safe components.",
+                    userProfiles: "Option to save personal preferences for different users."
+                }
+            },
+            pmc02: {
+                title: "title",
+                details: "details",
+                price: 100,
+                imagePath: "",
+                keySpecs : {
+                    coffeeTypes: "Espresso, Coffee, Americano, Latte Macchiato, Cappuccino, and more.",
+                    milkSystem: "LatteGo, which includes a two-part milk system for easy cleaning.",
+                    coffeeStrengthSettings: "Adjustable from mild to strong.",
+                    Grinder: "Integrated ceramic grinder with 12 settings for customizable grind size.",
+                    waterTankCapacity: "Approximately 18.5L",
+                    beanContainerCapacity: "About 250g",
+                    Dimensions: " Approximately 24 x 37 x 43 cm (W x D x H).",
+                    Weight: "Around 7.5 kg.",
+                    powerConsumption: "1500 watts."
+                },
+                features: {
+                    oneTouchFunctionality: "Allows for quick preparation of various coffee drinks.",
+                    easyCleaning: "Removable brewing group and dishwasher-safe components.",
+                    userProfiles: "Option to save personal preferences for different users."
+                },
+            },
+            pmc03: {
+                title: "title",
+                details: "details",
+                price: 100,
+                imagePath: "",
+                keySpecs : {
+                    coffeeTypes: "Espresso, Coffee, Americano, and more.",
+                    milkSystem: "None",
+                    coffeeStrengthSettings: "Adjustable from mild to strong.",
+                    Grinder: "Integrated ceramic grinder with 12 settings for customizable grind size.",
+                    waterTankCapacity: "Approximately 18.5L",
+                    beanContainerCapacity: "About 250g",
+                    Dimensions: " Approximately 24 x 37 x 43 cm (W x D x H).",
+                    Weight: "Around 7.5 kg.",
+                    powerConsumption: "1500 watts."
+                },
+                features: {
+                    oneTouchFunctionality: "Allows for quick preparation of various coffee drinks.",
+                    easyCleaning: "Removable brewing group and dishwasher-safe components.",
+                    userProfiles: "Option to save personal preferences for different users."
+                }
+            },
+            pmc04: {
+                title: "title",
+                details: "details",
+                price: 100,
+                imagePath: "",
+                keySpecs : {
+                    coffeeTypes: "Espresso, Coffee, Americano, and more.",
+                    milkSystem: "None",
+                    coffeeStrengthSettings: "Adjustable from mild to strong.",
+                    Grinder: "Integrated ceramic grinder with 12 settings for customizable grind size.",
+                    waterTankCapacity: "Approximately 18.5L",
+                    beanContainerCapacity: "About 250g",
+                    Dimensions: " Approximately 24 x 37 x 43 cm (W x D x H).",
+                    Weight: "Around 7.5 kg.",
+                    powerConsumption: "1500 watts."
+                },
+                features: {
+                    oneTouchFunctionality: "Allows for quick preparation of various coffee drinks.",
+                    easyCleaning: "Removable brewing group and dishwasher-safe components.",
+                    userProfiles: "Option to save personal preferences for different users."
+                }
+            }
+        },
+    }
+
+module.exports = data
